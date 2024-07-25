49385c3eea99ea15099331edff91884ce429f1a3
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2024-07-24 22:21:13 -0300

    Add files via upload

---
+++ Compras/src/CartaoDeCredito.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private double limite;
+    private double saldo;
+    private List<Compra> compras;
+
+    // criação do construtor
+    public CartaoDeCredito(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    // método de lançamento de compra
+    public boolean lancaCompra(Compra compra) {
+        if (this.saldo > compra.getValor()) {
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+
+    // getters
+    public double getLimite() {
+        return limite;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+}
